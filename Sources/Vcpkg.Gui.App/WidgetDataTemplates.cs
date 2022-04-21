c0adf2460d3629f98656f78d74dc1096d8e44a71
Wang Kai <stdcpp@outlook.com>
2022-04-21 23:47:58 +0800

    It is not completed.

--- Sources/Vcpkg.Gui.App/WidgetDataTemplates.cs
+++ Sources/Vcpkg.Gui.App/WidgetDataTemplates.cs
@@ -15,103 +15,53 @@ namespace Vcpkg.Gui.App
 {
     public static class WidgetDataTemplates
     {
-        public static FuncDataTemplate<IPackageInfo> MenuItemTemplate { get; } = _createMenuItemTemplate();
-        private static FuncDataTemplate<IPackageInfo> _createMenuItemTemplate()
-        {
-            var dt = new FuncDataTemplate<IPackageInfo>(
-                (pi, scope) =>
-                {
-                    var nameTb = new TextBlock
-                    {
-                        [!TextBlock.TextProperty] = new Binding("Name")
-                        {
-                            Converter = new NameConverter()
-                        },
-                    };
-                    var versionTb = new TextBlock
-                    {
-                        [!TextBlock.TextProperty] = new Binding("Version")
-                        {
-                            Converter = new VersionConverter()
-                        },
-                    };
-                    var portVersionTb = new TextBlock
-                    {
-                        [!TextBlock.TextProperty] = new Binding("PortVersion")
-                        {
-                            Converter = new PortVersionConverter()
-                        },
-                    };
-                    var descriptionTb = new TextBlock
-                    {
-                        TextWrapping = TextWrapping.WrapWithOverflow,
-                        [!TextBlock.TextProperty] = new Binding("Description")
-                        {
-                            Converter = new DescriptionConverter()
-                        },
-                    };
-                    var briefPanel = new StackPanel()
-                    {
-                        Children =
-                        {
-                            nameTb,
-                            versionTb,
-                            portVersionTb,
-                            descriptionTb,
-                        }
-                    };
-                    return briefPanel;
-                });
-
-            return dt;
-        }
         public static FuncDataTemplate<IPackageInfo> PackageInfoItem { get; } = _createPackageInfoItemTemplate();
         private static FuncDataTemplate<IPackageInfo> _createPackageInfoItemTemplate()
         {
             var dt = new FuncDataTemplate<IPackageInfo>(
-                (pi, scope) =>
-                {
-                    var nameTb = new TextBlock
-                    {
-                        [!TextBlock.TextProperty] = new Binding("Name")
-                        {
-                            Converter = new NameConverter()
-                        },
-                    };
-                    var versionTb = new TextBlock
-                    {
-                        [!TextBlock.TextProperty] = new Binding("Version")
-                        {
-                            Converter = new VersionConverter()
-                        },
-                    };
-                    var portVersionTb = new TextBlock
-                    {
-                        [!TextBlock.TextProperty] = new Binding("PortVersion")
-                        {
-                            Converter = new PortVersionConverter()
-                        },
-                    };
-                    var descriptionTb = new TextBlock
-                    {
-                        TextWrapping = TextWrapping.WrapWithOverflow,
-                        [!TextBlock.TextProperty] = new Binding("Description")
-                        {
-                            Converter = new DescriptionConverter()
-                        },
-                    };
-                    var briefPanel = new StackPanel()
-                    {
-                        Children =
-                        {
+               (pi, scope) =>
+               {
+                   var nameTb = new TextBlock
+                   {
+                       [!TextBlock.TextProperty] = new Binding(nameof(IPackageInfo.Name))
+                       {
+                           Converter = new NameConverter()
+                       },
+                   };
+                   var versionTb = new TextBlock
+                   {
+                       [!TextBlock.TextProperty] = new Binding(nameof(IPackageInfo.Version))
+                       {
+                           Converter = new VersionConverter()
+                       },
+                   };
+                   var portVersionTb = new TextBlock
+                   {
+                       [!TextBlock.TextProperty] = new Binding(nameof(IPackageInfo.PortVersion))
+                       {
+                           Converter = new PortVersionConverter()
+                       },
+                   };
+                   var descriptionTb = new TextBlock
+                   {
+                       TextWrapping = TextWrapping.WrapWithOverflow,
+                       [!TextBlock.TextProperty] = new Binding(nameof(IPackageInfo.Description))
+                       {
+                           Converter = new DescriptionConverter()
+                       },
+                   };
+                   var briefPanel = new StackPanel()
+                   {
+                       Children =
+                       {
                             nameTb,
                             versionTb,
                             portVersionTb,
                             descriptionTb,
-                        }
-                    };
-                    return briefPanel;
-                });
+                       }
+                   };
+                   return briefPanel;
+               });
 
             return dt;
         }
